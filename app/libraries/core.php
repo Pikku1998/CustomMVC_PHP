5115b141227832dc1291d29fa5b2e8dcec661c2c
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-14 17:35:28 +0530

    instantiating default controller

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -1,9 +1,33 @@
 <?php
 class Core{
+
+    protected $currentController = 'Pages';
+
+
     public function __construct(){
-        $this->getUrl();
+        $url = $this->getUrl();
+
+        $controller = ucwords($url[0]); //extracts controller from url array and capitalises first letter.
+        
+        // checks if the controller exists, if Yes, assigns it to currentController
+        if(file_exists('../app/controllers/'.$controller.'.php')){
+            $this->currentController = $controller;
+        };
+
+        require_once '../app/controllers/'. $this->currentController. '.php';
+
+        // Instatiate current controller
+        new $this->currentController;
     }
+
+
+
     public function getUrl(){
-        echo $_GET['url'];
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'],'/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/',$url);
+            return $url;
+        }
     }
 }
