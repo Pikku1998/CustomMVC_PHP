1b37e50690e27b78786c5a77f39b478bcc9a98d8
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-15 21:51:03 +0530

    enhancing core.php file

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -9,24 +9,32 @@ class Core{
     public function __construct(){
         $url = $this->getUrl();
 
-        $controller = ucwords($url[0]); //extracts controller from url array and capitalises first letter.
+        // checks to see if controller is passed in url, else defaultcontroller is loaded.
+        if(isset($url)){
+            $controller = ucwords($url[0]); //extracts controller from url array and capitalises first letter.
         
-        // checks if the controller exists, if Yes, assigns it to $currentController
-        if(file_exists('../app/controllers/'.$controller.'.php')){
-            $this->currentController = $controller;
-        };
+            // checks if the controller exists, if Yes, assigns it to $currentController
+            if(file_exists('../app/controllers/'.$controller.'.php')){
+                $this->currentController = $controller;
+            };
+        }
+
 
         require_once '../app/controllers/'. $this->currentController. '.php';
 
         // Instatiate current controller
         $this->currentController = new $this->currentController;
 
-        $method = $url[1];  // extract method from url array
+        // checks to see if method is passed in url, else defaultmethod is loaded.
+        if(isset($url[1])){
+            $method = $url[1];  // extract method from url array
 
-        // if method exists, assign it to $currentMethod
-        if(method_exists($this->currentController, $method)){
-            $this->currentMethod = $method;
+            // if method exists, assign it to $currentMethod
+            if(method_exists($this->currentController, $method)){
+                $this->currentMethod = $method;
+            }
         }
+
 
         // Extract params from url array
         $this->params = !empty($url[2]) ? $url[2] : '';
